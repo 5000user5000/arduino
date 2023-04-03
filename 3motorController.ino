94e5e9247691be03ed229f8958350289e1fc969a
Wayne <37620231+5000user5000@users.noreply.github.com>
2023-04-03 10:46:26 +0800

    debug

--- 3motorController.ino
+++ 3motorController.ino
@@ -6,7 +6,7 @@
 
 const int stepsPerRevolution = 2048;        //設定步進馬達轉一圈為 2048步
 
-Stepper myStepper = Stepper(stepsPerRevolution, 11, 9, 10, 8);       //因為步進馬達28BYJ-48 和程式庫 Stepper.h 的要求，這𥚃 9 和 10 不可互調 
+Stepper myStepper = Stepper(stepsPerRevolution, 11, 9, 10, 8);       //因為步進馬達28BYJ-48 和程式庫 Stepper.h 的要求，順序為 pin1/pin3/pin2/pin4
 Servo servo1;  //控制車體方向
 Servo servo2;  //控制機翼升降
 
@@ -21,10 +21,16 @@ Servo servo2;  //控制機翼升降
 
 #define motorSpeed 15 //馬達轉速(PRM)
 
+//函數宣告
+void initial(int delaytime=5000);
+void Stepper_forward(int steps,int delaytime=1000);
+void Servo_turn(int angle,int delaytime=1000);
+void Servo_updown(int angle,int delaytime=1000);
+
 void setup() {
     servo1.attach(servo1Pin);
     servo2.attach(servo2Pin);
-    myStepper.setSpeed(motorSpeed);   // 角速度(PRM)，範圍0~15。這裡設定為15，表示每分鐘轉15圈。(這裡還可以在測試如果插上電源的話的最大值)
+    myStepper.setSpeed(motorSpeed);   // 角速度(PRM)，範圍0~15。這裡設定為15，表示每分鐘轉15圈。(這裡還可以在測試如果插上電源的話的最大值，max 20)
 }
 
 void loop() {
